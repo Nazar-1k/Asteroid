7a8bb3605dbda79e9d097df29f53adabd5b8870f
Nazar Maksymenko <na.programmer.z@gmail.com>
2023-02-13 21:14:51 +0200

    c++

--- Asteroid/Asteroid/src/main.cpp
+++ Asteroid/Asteroid/src/main.cpp
@@ -3,7 +3,7 @@ and may not be redistributed without written permission.*/
 
 //Using SDL and standard IO
 #include <SDL.h>
-#include <stdio.h>
+#include <iostream>
 
 //Screen dimension constants
 const int SCREEN_WIDTH = 640;
